bb40e7ad3416814ad40b06be8ac933b6e68f5620
Mateus Fortes <mateus.kras.fortes@gmail.com>
2023-08-11 19:56:56 -0300

    a

--- assets/main-2.js
+++ assets/main-2.js
@@ -1,6 +1,7 @@
 const equiz = document.querySelector(".quiz")
 const epergunta = equiz.querySelector(".pergunta")
 const eresposta = equiz.querySelector(".resposta")
+const fim = document.querySelector("#fim")
 
 const quiz = [
     {
@@ -12,12 +13,26 @@ const quiz = [
         "pergunta" : "Qual é o nome da empresa criadora do jogo?",
         "alternativa" : ["LG","ubisoft","Rockstar","CDProjectRed"],
         "resposta" : 3
+    },
+    {
+        "pergunta" : "Qual ator renomado emprestou sua voz e semelhança ao personagem Johnny Silverhand em Cyberpunk 2077?",
+        "alternativa" : ["Keanu Reeves","Robert Downey Jr.","Tom Cruise","Chris Hemsworth"],
+        "resposta" : 0
+    },
+    {
+        "pergunta" : "Qual é a cidade principal onde se passa a maior parte do jogo Cyberpunk 2077?",
+        "alternativa" : ["Los Angeles","Tóquio","Night City","Neo York"],
+        "resposta" : 2
     }
 ]
 
 let npergunta = 0
+let c = 0
 
 function pergunta(np) {
+    if (c >= 4) {
+        fim.style.display = "block"
+    }
     epergunta.innerHTML = quiz[np].pergunta
     eresposta.innerHTML = ""
     quiz[npergunta].alternativa.forEach(alt => eresposta.innerHTML += `<button>${alt}</button>`)
@@ -28,6 +43,7 @@ const alternativaClicada = ev.target;
 const arrayAlternativas = [...eresposta.children]
 const Clicada = arrayAlternativas.indexOf(alternativaClicada)
 if (Clicada == quiz[npergunta].resposta) {
+    ++c
     pergunta(++npergunta)
     return
 }
